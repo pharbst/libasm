6c42895bb4b11ea279520a7d5f3d3521890cb113
pharbst <pharbst@student.42heilbronn.de>
2025-04-29 08:52:56 +0200

    strdup written

---
+++ strdup.s
@@ -0,0 +1,18 @@
+eextrn malloc
+seection .text
+	global _strdup
+
+	# rdi -> string to duplicate
+
+_strdup:
+	push rdi		; save rdi original pointer to the stack
+	call _strlen	; call strlen
+	inc rax			; incremnt for 0 byte
+	mov rdi, rax	; put return of strlen into rdi
+	call malloc		; call malloc
+	mov rdi, rax	; store malloc result in rdi for strcpy
+	pop rsi			; get the original pointer of the string to duplicate in rsi
+	push rax		; save the original pointer from malloc onto the stack
+	call _strcpy	; call strcpy with rdi deestination pointer and rsi src pointer
+	pop rax			; get back the original pointer from malloc
+	ret				; return the pointer to the duplicated string
