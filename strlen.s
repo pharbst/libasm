c7673c4bd4c02a30986a1e4ce11bb2cfb1bc64b3
pharbst <pharbst@student.42heilbronn.de>
2025-04-29 06:30:39 +0200

    added comments for all instructions

--- strlen.s
+++ strlen.s
@@ -2,12 +2,12 @@ section .text
     global _strlen
 
 _strlen:
-    xor rax, rax
+    xor rax, rax            ; set rax to null
     .loop:
-        cmp byte [rdi], 0
-        je .return
-        inc rdi
-        inc rax
-        jmp .loop
+        cmp byte [rdi], 0   ; compare *rdi with 0
+        je .return          ; jump equal to return
+        inc rdi             ; increment rdi pointer
+        inc rax             ; increment counter
+        jmp .loop           ; jump back to .loop
     .return:
-        ret
+        ret                 ; return rax
