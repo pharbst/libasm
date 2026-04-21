4f2e0b1ad6a31d6fcafa10577549424055bc7d29
pharbst <102487579+pharbst@users.noreply.github.com>
2026-04-21 20:49:57 +0000

    refactor of ft_read

--- ft_read.s
+++ ft_read.s
@@ -3,20 +3,19 @@ extern errno_location
 	; rdi -> fd
 	; rsi -> char *buff
 	; rdx -> buffer size
-section.text
+
+section .text
 	global _ft_read
 _ft_read:
-	xor rax, rax
-	mov ebx, edi
-	mov ecx, esi
-	; mov edx, rdx
-	mov eax, 3
-	int 0x80
-	test rax, rax
-	jns .return
-	neg rax
-	mov rdi, rax
-	call errno_location
-	mov rax, -1
-	.return:
+	mov rax, SYS_READ
+	syscall
+	js .error
+	ret
+
+	.error
+		neg rax
+		mov edx, rax
+		call errno_location
+		mov [rax], edx
+		mov rax, -1
 		ret
