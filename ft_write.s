a20322524c5667d4704d6e0dc82ebd940df7887e
pharbst <102487579+pharbst@users.noreply.github.com>
2026-04-21 20:45:17 +0000

    refactor of ft_write

--- ft_write.s
+++ ft_write.s
@@ -7,16 +7,15 @@ extern errno_location
 section .text
 	global _ft_write		; global symbol
 _ft_write:
-	xor rax, rax
-	mov ebx, edi			; mov fd into ebx
-	mov ecx, esi			; mov char *buff into ecx
-	mov eax, 4				; set syscall number 4 for write
-	int 0x80				; interrupt and call kernel
-	test rax, rax			; check system call return >= 0
-	jns .return				; jump if signed flag is false means rax contains bytes written
-	neg rax					; negate result to get errno value
-	mov rdi, rax			; move errno value into rdi
-	call errno_location		; call errno to set error code
-	mov rax, -1				; set rax to -1
-	.return:
+	mov rax, SYS_WRITE
+	syscall
+	js .error
+	ret
+
+	.error
+		neg rax
+		mov edx, rax
+		call errno_location
+		mov [rax], edx
+		mov rax, -1
 		ret
